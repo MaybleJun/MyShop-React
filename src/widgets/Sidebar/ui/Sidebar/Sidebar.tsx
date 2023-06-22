16a0b6ac0f9115de895b0fa05d6eb0473dd831b0
Mayble Jun <mayblejul@gmail.com>
2023-06-22 16:06:03 +0400

    Add i18n, DefinePlugin, HotModuleReplacement, Babel, Babel Extract plugin

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,9 +2,12 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Sidebar.module.scss';
 import {useState} from "react";
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwitcher";
+
 interface SidebarProps {
     className?: string;
 }
+
 export const Sidebar = ({className}: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
 
@@ -14,11 +17,12 @@ export const Sidebar = ({className}: SidebarProps) => {
 
     return (
         <div
-            className={classNames (cls.Sidebar, {[cls.collapsed]: collapsed},[className])}
+            className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
         >
-         <button onClick={onToggle}>Toggle</button>
+            <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
