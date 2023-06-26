42a3f28a03d5af155d14b9c8a3a93d1de514c058
Mayble Jun <mayblejul@gmail.com>
2023-06-26 12:46:30 +0400

    Add Error Boundary, Bundle Analyzer.

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { BugButton } from 'app/providers/ErrorBoundary';
 
 const MainPage = () => {
-    const { t } = useTranslation('main');
+    const { t } = useTranslation();
 
     return (
         <div>
-            {t('Главная')}
+            <BugButton />
+            {t('Главная страница')}
         </div>
     );
 };
