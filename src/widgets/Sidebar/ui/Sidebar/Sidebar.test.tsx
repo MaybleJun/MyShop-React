96fc2c3fcac0835a78bc4c778ea6e216750a90f9
Mayble Jun <mayblejul@gmail.com>
2023-06-28 17:22:48 +0400

    Add react testing library, test for Button and Sidebar.

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,20 @@
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import {
+    renderWithTranslation,
+} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { Sidebar } from './Sidebar';
+
+describe('Sidebar', () => {
+    test('with only first param', () => {
+        renderWithTranslation(<Sidebar />);
+        waitFor(() => expect(screen.getAllByTestId('sidebar')).toBeInTheDocument());
+    });
+
+    test('test toggle', () => {
+        renderWithTranslation(<Sidebar />);
+        const toggleBtn = screen.getByTestId('sidebar-toggle');
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        fireEvent.click(toggleBtn);
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
+    });
+});
